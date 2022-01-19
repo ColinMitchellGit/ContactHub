c8b6fbbd97b7e9fa6764b847db5b5da0b8c30039
Colin <benhawk6003@gmail.com>
2022-01-19 09:58:25 -0500

    Fixed Register.php and finished CreateContact.php

--- LAMPAPI/CreateContact.php
+++ LAMPAPI/CreateContact.php
@@ -3,18 +3,19 @@
 
 	$firstName = $inData["firstName"];
 	$lastName = $inData["lastName"];
-	$login = $inData["login"];
-	$password = $inData["password"];
+	$phoneNumber = $inData["phoneNumber"];
+	$email = $inData["email"];
 
 	$conn = new mysqli("localhost", "TheBeast", "Group15LovesCOP4331", "COP4331Group15");
+
 	if ($conn->connect_error)
 	{
 		returnWithError( $conn->connect_error );
 	}
 	else
 	{
-		$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
-		$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+		$stmt = $conn->prepare("INSERT into Contacts (FirstName, LastName, PhoneNumber, Email) VALUES(?,?,?,?)");
+		$stmt->bind_param("ssis", $firstName, $lastName, $phoneNumber, $email);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
