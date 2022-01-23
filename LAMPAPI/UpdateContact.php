76b18ef387f96e4934a703084c95e8a2c4d78cbd
DarthJarJar56 <ssallam2013@gmail.com>
2022-01-22 20:22:46 -0500

    flipped syntax

--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -20,8 +20,8 @@
     else
     {
 		# There's no need to check if the contact exists if they are on the contact
-		$stmt = $conn->prepare("UPDATE `Contacts` SET `FirstName` = ?, `LastName` = ?, `PhoneNumber` = ?, `Email` = ? 
-                                WHERE (`FirstName` = ? AND `LastName` = ? AND `PhoneNumber` = ? AND `Email` = ?) AND (`UserID`=?)");
+		$stmt = $conn->prepare("UPDATE Contacts SET FirstName = ?, LastName = ?, PhoneNumber = ?, Email = ? 
+                                WHERE FirstName = ? AND LastName = ? AND PhoneNumber = ? AND Email = ? AND UserID = ?");
 		$stmt->bind_param("ssisssisi", $newFirstName, $newLastName, $newPhoneNumber, $newEmail, 
                                        $oldFirstName, $oldLastName, $oldPhoneNumber, $oldEmail, $userid);
 		$stmt->execute();
