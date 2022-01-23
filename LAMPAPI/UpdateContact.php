023605c3d42465168dde95e57e814239436eaf5e
DarthJarJar56 <ssallam2013@gmail.com>
2022-01-23 18:04:18 -0500

    changing indata

--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -1,15 +1,12 @@
 <?php
 	$inData = getRequestInfo();
 
-	$oldFirstName = $inData["oldFirstName"];
-	$oldLastName = $inData["oldLastName"];
-	$oldPhoneNumber = $inData["oldPhoneNumber"];
-	$oldEmail = $inData["oldEmail"];
-	$newFirstName = $inData["newFirstName"];
-	$newLastName = $inData["newLastName"];
-	$newPhoneNumber = $inData["newPhoneNumber"];
-	$newEmail = $inData["newEmail"];
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$phoneNumber = $inData["phoneNumber"];
+	$email = $inData["email"];
 	$userID = $inData["userID"];
+	$contactID = $inData["contactID"];
 
 	$conn = new mysqli("localhost", "TheBeast", "Group15LovesCOP4331", "COP4331Group15");
 
@@ -21,9 +18,9 @@
     {
 		# There's no need to check if the contact exists if they are on the contact
 		$stmt = $conn->prepare("UPDATE Contacts SET `FirstName`=?, `LastName`=?, `PhoneNumber`=?, `Email`=? 
-		                        WHERE `FirstName`=? AND `LastName`=? AND `PhoneNumber`=? AND `Email`=? AND `UserID`=?");
-		$stmt->bind_param("ssisssisi", $newFirstName, $newLastName, $newPhoneNumber, $newEmail, 
-                                       $oldFirstName, $oldLastName, $oldPhoneNumber, $oldEmail, $userid);
+		                        WHERE `UserID`=? AND `ContactID`=?");
+		$stmt->bind_param("ssisii", $firstName, $lastName, $phoneNumber, $email, 
+                                       $userid, $contactID);
 		$stmt->execute();
 
 		$result = $stmt->get_result();
