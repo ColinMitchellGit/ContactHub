1079e91f0a0e7a2f84c69661a15bc0049107edf0
DarthJarJar56 <ssallam2013@gmail.com>
2022-01-23 17:42:13 -0500

    Adding experimental script

---
+++ LAMPAPI/UpdateContact2.php
@@ -0,0 +1,66 @@
+<?php
+	$inData = getRequestInfo();
+
+	$oldFirstName = $inData["oldFirstName"];
+	$oldLastName = $inData["oldLastName"];
+	$oldPhoneNumber = $inData["oldPhoneNumber"];
+	$oldEmail = $inData["oldEmail"];
+	$newFirstName = $inData["newFirstName"];
+	$newLastName = $inData["newLastName"];
+	$newPhoneNumber = $inData["newPhoneNumber"];
+	$newEmail = $inData["newEmail"];
+	$userID = $inData["userID"];
+
+	$conn = new mysqli("localhost", "TheBeast", "Group15LovesCOP4331", "COP4331Group15");
+
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+    else
+    {
+		# There's no need to check if the contact exists if they are on the contact
+		$stmt = $conn->prepare("UPDATE Contacts SET `FirstName`=?, `LastName`=?, `PhoneNumber`=?, `Email`=? 
+		                        WHERE `FirstName`=? AND `LastName`=? AND `PhoneNumber`=? AND `Email`=? AND `UserID`=?");
+		$stmt->bind_param("ssisssisi", $newFirstName, $newLastName, $newPhoneNumber, $newEmail, 
+                                       $oldFirstName, $oldLastName, $oldPhoneNumber, $oldEmail, $userid);
+		$stmt->execute();
+        $result = $stmt->get_result();
+        
+        if ($result != TRUE)
+        {
+            returnWithError("Edit Failed");
+        }
+        else
+        {
+            returnInfo("Edit went through!");
+        }
+        
+        $stmt->close();
+        $conn->close();
+    }
+
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+    function returnInfo($info)
+    {
+        $retValue = '{"info":"' . $info . '"}';
+        sendResultInfoAsJson($retValue);
+    }
+?>
