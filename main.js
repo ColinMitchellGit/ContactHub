81466bee3af19ca6b3b1eadb034c8ad4b2791e94
mdobinda <66760972+mdobinda@users.noreply.github.com>
2022-01-23 22:48:10 -0500

    fixed a line
    sorry typo

--- main.js
+++ main.js
@@ -106,7 +106,7 @@ function doLogout()
     firstName = "";
     lastName = "";
     document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
-    window.location.href = "landing.html";
+    window.location.href = "cover.html";
 }
 
 function addColor()
