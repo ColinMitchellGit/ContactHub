b206078611539450f05315a77d6ee47d9cae1446
Colin <benhawk6003@gmail.com>
2022-01-24 14:27:54 -0500

    Fixing update contact script

--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -17,8 +17,10 @@
     else
     {
 		# There's no need to check if the contact exists if they are on the contact
-		$stmt = $conn->prepare("UPDATE Contacts SET FirstName=$firstName, LastName=$lastName, PhoneNumber=$phoneNumber, Email=$email
-		                        WHERE UserID=$userid AND ContactID=$contactID");
+		$stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, PhoneNumber=?, Email=?
+		                        WHERE UserID=? AND ContactID=?");
+		$stmt->bind_param("ssisii", $firstName, $lastName, $phoneNumber, $email,
+                                    $userID, $contactID);
 		$stmt->execute();
 
 
