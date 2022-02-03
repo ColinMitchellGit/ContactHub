b63bc4c34a3e55b31906405e1a7caafd17d89365
Colin <benhawk6003@gmail.com>
2022-02-03 18:57:16 -0500

    Testing modal show

--- main.js
+++ main.js
@@ -461,6 +461,8 @@ function doReadContacts()
 					button1.type = "button";
 					button1.className = "button1 mt-3 mb-5";
 					button1.onclick = function() {$('#myModal').modal('show');};
+					button1.setAttribute("data-bs-toggle", "modal");
+					button1.setAttribute("data-bs-target", "#myModal");
 					button1.innerHTML = "Edit";
 					data2.appendChild(button1);
 
