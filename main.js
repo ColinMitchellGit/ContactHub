da102709251287f1218d0dd10366b53eb45516d3
Colin <benhawk6003@gmail.com>
2022-02-03 13:20:32 -0500

    Testing contacts

--- main.js
+++ main.js
@@ -251,61 +251,6 @@ function doLogin()
     }
 }
 
-function doReadContacts()
-{
-    let searchTerm = document.getElementById("searchBar").value;
-
-    let tmp = {userID:userId,search:searchTerm};
-    let jsonPayload = JSON.stringify( tmp );
-
-    let url = urlBase + '/ReadContacts.' + extension;
-
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-
-    try
-    {
-		// We send the API query and wait for it to return.
-		// The API queries are "asynchronous", which means that other
-		// code can run in the background while the query is pending.
-		xhr.send(jsonPayload);
-
-		// Once the API query returns and is noted as ready, this function triggers.
-        xhr.onreadystatechange = function()
-        {
-			// This tells if the query has returned and everything is correct with it.
-            if (this.readyState === 4 && this.status === 200)
-            {
-				// Parsing the returned information from the query to a JSON object.
-                let jsonObject = JSON.parse( xhr.responseText );
-
-				if (jsonObject.error != "")
-				{
-					return;
-				}
-
-				let contactArray = jsonObject.results;
-
-				console.log(contactArray);
-				/*
-				for (var i = 0; i < contactArray.length; i++) {
-					let firstName = contactArray[i].firstName;
-					let lastName = contactArray[i].lastName;
-					let phoneNumber = contactArray[i].phoneNumber;
-					let email = contactArray[i].email;
-					let contactID = contactArray[i].contactID;
-				}*/
-            }
-        };
-    }
-    catch(err)
-    {
-		console.log(err);
-    }
-
-}
-
 function saveCookie()
 {
     let minutes = 20;
