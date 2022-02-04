6b98cc84ad9353a61cf03f686d8ec82277896966
Colin <benhawk6003@gmail.com>
2022-02-03 19:26:24 -0500

    Edit auto populating test

--- main.js
+++ main.js
@@ -475,7 +475,15 @@ function doReadContacts()
 					button1.setAttribute("data-bs-toggle", "modal");
 					button1.setAttribute("data-bs-target", "#myModal2");
 					button1.innerHTML = "Edit";
-                    button1.onclick = function() { globalContactID = contactID };
+                    button1.onclick = function()
+					{
+						globalContactID = contactID;
+
+						document.getElementById("newFirst").value = contactFirstName;
+					    document.getElementById("newLast").value = contactLastName;
+					    document.getElementById("newPhone").value = phoneNumber;
+					    document.getElementById("newEmail").value = email;
+					};
 					data2.appendChild(button1);
 
 					let button2 = document.createElement("button");
